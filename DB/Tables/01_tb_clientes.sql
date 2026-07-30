38fa11c4b70fad3f16681c4da65bb239f036813d
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-07-30 12:07:05 -0300

    feat: Atualizando Tabela de Clientes

--- DB/Tables/01_tb_clientes.sql
+++ DB/Tables/01_tb_clientes.sql
@@ -1,28 +1,64 @@
-DROP TABLE tb_clientes;
-
-
---Criação da tabela clientes 
-CREATE TABLE tb_clientes ( 
-	id_cliente VARCHAR(10) PRIMARY KEY DEFAULT ('C' || LPAD(NEXTVAL('id_cliente_sequence')::TEXT,3,'0')) REFERENCES tb_clientes(id_cliente),
-	nome_cliente VARCHAR(100) NOT NULL,
-	cnpj_cliente VARCHAR(18) NOT NULL UNIQUE,
-	id_segmento_cliente INTEGER REFERENCES tb_segmentos(id_segmento)
+-- =====================================================
+-- ================{TABELA_CLIENTES}====================
+-- =====================================================
+CREATE TABLE TB_CLIENTES (
+	ID SERIAL PRIMARY KEY,
+	NOME VARCHAR(100) NOT NULL,
+	CNPJ VARCHAR(18) NOT NULL UNIQUE,
+	SEGMENTO INT DEFAULT 1,
+	DATA_CRIACAO TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	DATA_MODIFICACAO TIMESTAMP,
+	CONSTRAINT FK_SEGMENTO FOREIGN KEY (SEGMENTO) REFERENCES TB_SEGMENTOS (ID_SEGMENTO)
 );
+-- =====================================================
+-- =================={FIM_Tabela}=======================
+-- =====================================================
 
---SELECT * FROM tb_clientes;
+-- //////////////////////////////////////////////////////
+-- ================{TRIGGER_FUNCTION}====================
+--///////////////////////////////////////////////////////
 
-INSERT INTO tb_clientes (nome_cliente,cnpj_cliente,id_segmento_cliente) 
-VALUES ('Empresa Alpha', '12.345.678/0001-90',1);
+--================{Trigger_Data_Modificacao}=================
+CREATE OR REPLACE FUNCTION FN_UPDATE_DT_MODIFICACAO_CLIENTE () RETURNS TRIGGER AS $$
+BEGIN
+	NEW.DATA_MODIFICACAO = NOW();
+	RETURN NEW;
+END;
+$$ LANGUAGE PLPGSQL;
 
-INSERT INTO tb_clientes (nome_cliente,cnpj_cliente,id_segmento_cliente) 
-VALUES ('Empresa Beta', '98.765.432/0001-10',2);
+CREATE TRIGGER TRG_UP_DT_MODIFICACAO_CLIENTE BEFORE
+UPDATE ON TB_CLIENTES FOR EACH ROW
+EXECUTE FUNCTION FN_UPDATE_DT_MODIFICACAO_CLIENTE ();
 
-INSERT INTO tb_clientes (nome_cliente,cnpj_cliente, id_segmento_cliente)
-VALUES ('Empresa Gama','45.678.901/0001-20',3);
+-- //////////////////////////////////////////////////////
+-- ================{FIM_TRIGGER_FUNCTION}=================
+-- ///////////////////////////////////////////////////////
 
-INSERT INTO tb_clientes (nome_cliente,cnpj_cliente,id_segmento_cliente)
-VALUES ('Empresa Delta', '67.890.123/0001-30', 4);
+-- ///////////////////////////////////////////////
+-- ================{PROCEDURE}====================
+--////////////////////////////////////////////////
+CREATE OR REPLACE PROCEDURE PR_CRIAR_CLIENTE (
+	NOME_CLIENTE VARCHAR,
+	CNPJ_CLIENTE VARCHAR,
+	SEGMENTO_CLIENTE INT DEFAULT NULL
+) LANGUAGE PLPGSQL AS $$
+	BEGIN
+		INSERT INTO TB_CLIENTES(NOME , CNPJ, SEGMENTO ) VALUES (NOME_CLIENTE::TEXT, CNPJ_CLIENTE::TEXT, SEGMENTO_CLIENTE);
+	END;
+	$$;
+--////////////////////////////////////////////////
+-- ================{FIM_PROCEDURE}=================
+--////////////////////////////////////////////////
 
---Consulta Cliente por Nome segmento 
---Adicionar View
-SELECT c.id_cliente, c.nome_cliente, c.cnpj_cliente, s.nome_segmento FROM tb_clientes c INNER JOIN tb_segmentos s ON c.id_segmento_cliente = s.id_segmento;
+
+-- ==========={VIEW_SIMPLES_CLIENTES}==========
+CREATE VIEW VW_CLIENTES AS
+	SELECT * FROM TB_CLIENTES;
+
+CREATE VIEW VW_CLIENTE_ORDER_ID AS 
+	SELECT * FROM TB_CLIENTES ORDER BY ID;
+
+CREATE VIEW VW_CLIENTE_ORDER_ALF AS 
+
+	SELECT * FROM TB_CLIENTES ORDER NOME;
+-- ======================={FIM_Views}=================

024f4c25a8f9b5cf2ab6b5d37ddbe4384fde9838
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-08-04 00:17:16 -0300

    Criação do indexador de nome dos segmentos

--- DB/Tables/02_tb_segmentos.sql
+++ DB/Tables/02_tb_segmentos.sql
@@ -1,12 +1,8 @@
---Tabelas de Referências (Lookup)
---Tabela de Segmentos Clientes
 CREATE TABLE TB_SEGMENTOS (
 	ID_SEGMENTO INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	NOME_SEGMENTO VARCHAR(100) NOT NULL UNIQUE
 );
 
--- index garante que o nome_segmento não seja adicionado de outras formas 
--- exemplo : TECNOLOGIA, tecnologia, ...
 CREATE UNIQUE INDEX idx_segmentos_nome
 ON tb_segmentos(LOWER(TRIM(NOME_SEGMENTO)));
 
@@ -25,7 +21,9 @@ VALUES
 	('Financeiro'),
 	('Agronegócio'),
 	('Jurídico');
+	
 SELECT
 	*
 FROM
 	TB_SEGMENTOS;
+	

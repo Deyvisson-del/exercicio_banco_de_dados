84df7e73e6b65760bd1453325b0dfce121d7c3ae
Deyvisson Rafael Lima da Silva <deyvisson.rsilva@sectec.local>
2026-08-03 22:54:18 -0300

    Atualização da tabelas segmentos

--- DB/Tables/02_tb_segmentos.sql
+++ DB/Tables/02_tb_segmentos.sql
@@ -2,8 +2,13 @@
 --Tabela de Segmentos Clientes
 CREATE TABLE TB_SEGMENTOS (
 	ID_SEGMENTO INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	NOME_SEGMENTO VARCHAR(50) NOT NULL UNIQUE
+	NOME_SEGMENTO VARCHAR(100) NOT NULL UNIQUE
 );
+
+-- index garante que o nome_segmento não seja adicionado de outras formas 
+-- exemplo : TECNOLOGIA, tecnologia, ...
+CREATE UNIQUE INDEX idx_segmentos_nome
+ON tb_segmentos(LOWER(TRIM(NOME_SEGMENTO)));
 
 INSERT INTO
 	TB_SEGMENTOS (NOME_SEGMENTO)

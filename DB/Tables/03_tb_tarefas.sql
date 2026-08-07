d2cd1e781972381dbe14b61ea7e38ad4d8724869
Deyvisson-del <deyvissonrafael018@gmail.com>
2026-08-07 14:55:18 -0300

    Criação de view de tarefas

--- DB/Tables/03_tb_tarefas.sql
+++ DB/Tables/03_tb_tarefas.sql
@@ -16,4 +16,25 @@ CREATE TABLE TB_TAREFAS (
 	CONSTRAINT FK_ID_PRIORIDADE FOREIGN KEY (PRIORIDADE_ID) REFERENCES TB_PRIORIDADE_TAREFAS(ID_PRIORIDADE)
 );
 
+create or replace view vw_Tarefas as 
+select 
+	tt.id_tarefas as "ID tarefa",
+	tp.nome_projeto as "Nome Projeto",
+	tt.nome_tarefas as "Nome Tarefas",
+	tt.descricao_tarefas as "Descrição Tarefa",
+	tf.nome_funcionario as "Criador da tarefa",
+	tff.nome_funcionario as "Técnico",
+	tpt.nome_prioridade as "Prioridade",
+	tst.nome_status as "Status",
+	tt.data_criacao_tarefas as "Início Tarefa",
+	tt.data_finalizacao as "Fim Tarefa"
+from tb_tarefas tt  
+inner join tb_projetos tp on tt.projeto_id = tp.id_projeto
+inner join tb_funcionarios tf on requerente_id = tf.id_funcionario 
+inner join tb_funcionarios tff on funcionario_id = tff.id_funcionario
+inner join tb_prioridade_tarefas tpt on tt.prioridade_id = tpt.id_prioridade
+inner join tb_status_tarefas tst on tt.status_id = tst.id_status
+
+
+select * from vw_tarefas vt 
  
